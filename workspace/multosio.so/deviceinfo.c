7f0f7e46062569076093568e9860f0c71735e718
C Torr <chris.torr@multos.com>
2021-05-28 16:20:31 +0100

    File header update

--- workspace/multosio.so/deviceinfo.c
+++ workspace/multosio.so/deviceinfo.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020, MAOSCO Ltd
+ * Copyright (c) 2020-2021, MULTOS Ltd
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
  * following conditions are met:
