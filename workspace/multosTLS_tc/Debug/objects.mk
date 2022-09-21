13bca4f76770c24221643612a827851fd6b774f8
maosco <30289542+maosco@users.noreply.github.com>
2022-09-21 13:37:45 +0100

    Add missing file to link objects

--- workspace/multosTLS_tc/Debug/objects.mk
+++ workspace/multosTLS_tc/Debug/objects.mk
@@ -2,7 +2,7 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-USER_OBJS := /home/pi/workspace/cryptoki.so/Debug/tc_api.o
+USER_OBJS := /home/pi/workspace/cryptoki.so/Debug/tc_api.o /home/pi/workspace/p11keygen/Debug/base64.o
 
 LIBS :=
 
